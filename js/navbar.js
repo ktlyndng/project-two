5438e1e2676db8d7aa76b8d2b4855c385746c3cf
Kaitlyn Dang <kaitlyndang@Kaitlyns-MacBook-Pro-2.local>
2020-10-24 17:55:02 -0400

    created first two cards on food section

--- js/navbar.js
+++ js/navbar.js
@@ -5,16 +5,40 @@ var i;
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function () {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
     this.classList.toggle("active");
-
-    /* Toggle between hiding and showing the active panel */
     var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
     } else {
-      panel.style.display = "block";
+      panel.style.maxHeight = panel.scrollHeight + "px";
     }
   });
 }
+
+/* Navbar */
+
+// get the hamburger element
+var hamburger = document.getElementById("hamburger");
+
+// get the menu element
+var menu = document.getElementById("menu");
+
+// get the overlay element
+var overlay = document.getElementById("overlay");
+// define toggle function
+function toggleMenu() {
+  // either adds or removes active class from element
+  if (window.innerWidth > 768) return false;
+  menu.classList.toggle("active");
+  overlay.classList.toggle("active");
+}
+// execute toggle function from hamburger on click
+hamburger.addEventListener("click", toggleMenu);
+
+// execute toggle function from menu on click
+menu.addEventListener("click", toggleMenu);
+
+// exuecute toggle function from overlay on click
+overlay.addEventListener("click", toggleMenu);
+
+/* Navbar */
